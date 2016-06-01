d106a9ecbf47e0793381092daecf05a8b9c6a0d5
Iulii <iuliuvisovan@gmail.com>
2016-06-01 21:43:03 +0300

    Opacity smoothing on circle hover + detail level + color picker

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 'use strict'
 
 var iuliudotnetModule = angular.module('iuliudotnetModule', []);
+var coolColors = ['#1abc9c', '#3498db', '#34495e', '#e74c3c', '#2c3e50', '#27ae60', '#16a085', '#e67e22', '#2e8ece'];
+var currentThemeColor = '#1abc9c';
 
 iuliudotnetModule.controller('IndexController', function ($scope) {
-    $scope.squares = new Array(100 * 36);
+    var width = $(window).width() / 15;
+    var height = $(window).height() / 15;
+    $scope.squares = new Array(Math.floor(width * height));
     $scope.squareStyle = {
         "width": $(window).width() / 30,
         "height": $(window).width() / 30
@@ -13,18 +17,56 @@ iuliudotnetModule.controller('IndexController', function ($scope) {
         $scope.squareStyle.height = $(window).width() / 30;
         $scope.$apply();
     });
+    $scope.colors = coolColors;
+    $scope.currentColor = currentThemeColor;
+    $scope.coolness = 2;
+    $scope.$watch('coolness', function (newValue) {
+        if (newValue < 3) {
+            removeTransition($(".square"));
+            removeTransition($(".filling-rectangle"));
+        }
+        else {
+            addTransition($(".square"));
+            addTransition($(".filling-rectangle"));
+        }
+    });
+    $scope.currentColor = coolColors[new Date().getTime() % 9];
+    changeColorTheme($scope.currentColor);
+    setTimeout(function () {
+        $scope.coolness = 3;
+        $scope.$apply();
+    }, 500);
+
 });
+
+function removeTransition(item) {
+    item.css('transition', '');
+    item.css('-webkit-transition', '');
+    item.css('-o-transition', '');
+}
+
+function addTransition(item) {
+    item.css('transition', 'all 0.5s ease-in-out');
+    item.css('-webkit-transition', 'all 0.5s ease-in-out');
+    item.css('-o-transition', 'all 0.5s ease-in-out');
+}
 
 
 function calculateDistance(elem, mouseX, mouseY) {
     return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
 }
 
-$(document).ready(function () {
-    var coolColors = ['#1abc9c', '#3498db', '#34495e', '#e74c3c', '#2c3e50', '#27ae60', '#16a085', '#e67e22', '#2e8ece'];
+function changeColorTheme(color) {
+    $('body').removeClass('random-color');
     $('body').addClass('random-color');
-    setTimeout(function() {
-        $('.random-color').css("background-color", coolColors[new Date().getTime() % 9]);
+    $("#favicon").attr('href', 'img/logo_' + color.substr(1) + '.png');
+    setTimeout(function () {
+        $('.random-color').css("background-color", color);
+    }, 1000);
+}
+
+$(document).ready(function () {
+    setTimeout(function () {
         $(".menu-item").hover(function () {
                 $(this).siblings(".menu-item").find(".filling-rectangle").css("-webkit-filter", "opacity(60%)");
             },
@@ -42,7 +84,8 @@ $(document).ready(function () {
         var randSize = Math.floor(Math.random() * 15) + 10;
         $(this).css("width", randSize % 3 == 0 ? "10" : randSize);
         $(this).css("height", randSize % 3 == 0 ? "10" : randSize);
-    });1
+    });
+    1
 
     $(".square").mouseout(function (item) {
         var self = $(this);
