7f894afc2a62cb5f35e3048ef71ba94dc675b3d0
iuliuusource <iuliu.visovan@usource.ro>
2020-03-16 00:57:16 +0200

    add last 7 days cases

--- corona/js/app.js
+++ corona/js/app.js
@@ -8,7 +8,8 @@ function draw() {
     document.getElementById('countryPicker').value = 'Italy';
   }, 0);
 
-  drawTotalsChart('otherCountryChart', 'Italy');
+  drawTotalsChart();
+  drawLastWeekChart();
 }
 
 var otherCountryChart = undefined;
@@ -93,6 +94,65 @@ function drawTotalsChart() {
           data: values,
           backgroundColor: '#E91E6322',
           borderColor: '#E91E63',
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        height: 500,
+        padding: {
+          top: 0
+        }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              max: Math.ceil((maxValue + maxValue / 10) / 10) * 10
+            }
+          }
+        ]
+      }
+    }
+  });
+}
+
+function drawLastWeekChart() {
+  const ctx = document.getElementById('lastWeekTotals').getContext('2d');
+  const data = window.data;
+
+  const validData = data.filter(x => moment().diff(moment(x.DateRep), 'days') <= 7);
+
+  const totals = {};
+
+  validData.forEach(x => {
+    totals[x.CountryExp] = validData
+      .filter(y => y.CountryExp == x.CountryExp)
+      .map(x => x.NewConfCases)
+      .reduce((a, b) => +a + +b);
+  });
+
+  const countriesWithTotals = Object.keys(totals).map(key => ({ countryName: key, total: totals[key] }));
+
+  const sortedByTotalCases = countriesWithTotals.sort((a, b) => b.total - a.total).slice(0, 10);
+
+  const labels = [...new Set(sortedByTotalCases.map(x => x.countryName))];
+  const values = sortedByTotalCases.map(x => x.total);
+  const maxValue = Math.max(...values);
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels.map(x => (x.startsWith('Cases on an') ? 'Diamond Princess' : x)),
+      datasets: [
+        {
+          label: 'Cazuri in ultimele 7 zile',
+          data: values,
+          backgroundColor: '#9c27b022',
+          borderColor: '#9c27b0',
           borderWidth: 1
         }
       ]
