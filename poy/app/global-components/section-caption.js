ab39cbc44436e873fc85fb1c1e082d0d98cc9e45
unknown <iuliuvisovan@gmail.com>
2020-04-05 12:03:04 +0300

    remove some projects + fix /v link

--- poy/app/global-components/section-caption.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Label, vw } from '../global'
-
-export default (props) => (
-  <Label weight={500} style={{ padding: 15, color: '#7c878e', paddingBottom: 15, paddingTop: 25, backgroundColor: '#131e2a', width: vw }}>
-    {props.text.toUpperCase()}
-  </Label>
-)
