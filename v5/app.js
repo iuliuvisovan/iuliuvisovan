b4b9fc2ec232d03d21b7675bccb5617f0cdf54b0
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-08-23 16:59:40 +0300

    first fiew hovered

--- v5/app.js
+++ v5/app.js
@@ -1,14 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => { 
-  const logos = document.querySelectorAll('.logo');
-  console.log('logos', logos);
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const logos = document.querySelectorAll('.logo img');
+    console.log('logos', logos);
 
-  [...logos].forEach(x => {
-    console.log('x',x);
-    
-    const currentRotation = x.style;
+    [...logos].forEach((x) => {
+      x.addEventListener('mouseover', () => {
+        logos.forEach((y) => {
+          y.classList.add('greyed');
+          y.src = y.src.replace('-hovered', '');
+        });
 
-    console.log('currentRotation', currentRotation);
-    
-  })
-  
-}, false);
+        x.src = x.src.replace('.png', '-hovered.png');
+      });
+
+      x.addEventListener('mouseleave', () => {
+        logos.forEach((y) => {
+          y.src = y.src.replace('-hovered', '');
+        });
+      });
+    });
+  },
+  false
+);
