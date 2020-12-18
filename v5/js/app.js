40ccfa6478f8a85e498c4aa91e8248fecced2c09
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-12-18 02:18:26 +0200

    first implementation, but too low res

--- v5/js/app.js
+++ v5/js/app.js
@@ -1,28 +1,38 @@
+const catGifs = [
+  'https://i.pinimg.com/originals/ac/f9/a5/acf9a5214d40c8d911bd236b037f8b66.gif',
+  'https://media1.giphy.com/media/8zdi3lQp3fzKU/giphy.gif',
+  'https://i.makeagif.com/media/4-18-2018/0nyw45.gif',
+  'https://media.tenor.com/images/33845bc4ed116b73a6815d9cf8873972/tenor.gif',
+  'https://www.sadanduseless.com/wp-content/uploads/2019/07/cat-vs-cucumber3.gif',
+  'https://i.pinimg.com/originals/25/58/cc/2558cc0f605acfa7a31d563030723c96.gif',
+  'https://media1.giphy.com/media/ICOgUNjpvO0PC/giphy.gif',
+  'https://i.imgur.com/2JZKwrO.gif',
+  'https://kittybloger.files.wordpress.com/2014/03/25-funny-cat-gifs-15.gif',
+];
+
 setTimeout(() => {
-  const firstVideo = document.getElementById('first');
-
-  const perfectRatio = 16 / 9;
-
-  const actualRatio = window.innerWidth / window.innerHeight;
-
-  console.log('window.innerWidth', window.innerWidth);
-  console.log('window.innerHeight', window.innerHeight);
-
-  const totalWidth = window.innerWidth;
-  console.log('firstVideo.clientWidth', firstVideo.clientWidth);
-
-  const leftSpace = totalWidth - firstVideo.clientWidth;
-  console.log('leftSpace', leftSpace);
-
-  const halfLeftSpace = leftSpace / 2;
-  console.log('halfLeftSpace', halfLeftSpace);
-
-  const marginLeft = -1 * firstVideo.clientWidth + halfLeftSpace;
-
-  console.log('marginLeft', marginLeft);
-
-//   firstVideo.style.marginLeft = `${marginLeft}px`;
+  enterWebsite();
 }, 500);
+
+var currentGif = 0;
+
+function showNextGif() {
+  currentGif++;
+  if (currentGif > 7) {
+    currentGif = 0;
+  }
+  document.getElementById('catgif').src = catGifs[currentGif];
+}
+
+const videoElement = document.getElementById('mainVid');
+
+videoElement.addEventListener('loadeddata', () => {
+  setTimeout(() => {
+    document.getElementById('loadingText').remove();
+    document.getElementById('loadedText').classList.add('visible');
+    document.getElementById('showCatGifsLink').innerText = 'Actually I think some cat gifs would be nice';
+  }, 600);
+});
 
 setTimeout(() => {
   const vids = [...document.getElementsByTagName('video')];
@@ -35,3 +45,27 @@ setTimeout(() => {
   const vid = document.getElementById('mainvid');
   vid.pause();
 }, 4000);
+
+function enterWebsite() {
+  document.getElementById('loadingOverlay').remove();
+  document.getElementById('homepage').classList.add('entered');
+
+  setTimeout(() => {
+    document.getElementById('mainVid').play();
+    setTimeout(() => {
+      document.getElementById('mainVid').pause();
+      [...document.getElementsByClassName('trigger')].forEach((trigger) => {
+        trigger.classList.add('available');
+      });
+    }, 2132);
+  }, 1500);
+}
+
+setTimeout(() => {
+  document.getElementById('paperTrigger').addEventListener('mouseenter', function () {
+    document.getElementById('pageWrapper').style.transform = 'scale(1.3) translateY(-100px)';
+  });
+  document.getElementById('paperTrigger').addEventListener('mouseleave', function () {
+    document.getElementById('pageWrapper').style.transform = 'scale(1) translateY(0px)';
+  });
+}, 0);
